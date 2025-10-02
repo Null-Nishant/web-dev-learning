81d696f13daa831920176dfe20297d21c2158467
Null_Nishant <nsingh9430@gmail.com>
2025-10-02 12:46:15 +0530

    Learn Number, Math and random method

---
+++ js/day3.js
@@ -0,0 +1,43 @@
+//Number & Maths
+
+let a=42.1237
+console.log(a.toFixed(3));  //return string and fix upto given argument
+console.log(typeof a.toPrecision(4))
+
+console.log(a.toString());
+console.log(String(a));
+
+// Maths in js
+
+console.log(Math.PI)
+console.log(Math.log10(32))
+console.log(Math.sqrt(2))
+console.log(Math.floor(32.1))
+console.log(Math.ceil(32.1))
+console.log(Math.max(12,12,3,1,2))
+console.log(Math.min(121.3, 2, 1, 3, 1, 2))
+
+// Math.random() method is not safe because it uses time 
+
+//OTP Generator
+function OTP(length) {
+  let otp = "";
+  for (let i = 0; i < length; i++) {
+    otp += Math.floor(Math.random() * 9) + 1;
+  }
+  return Number(otp);
+}
+let lengthOfOtp = 4;
+console.log(OTP(lengthOfOtp));
+
+//for fixed lenght
+function GenOTP() {
+  return Math.floor(Math.random() * (9999 - 1000 + 1)) + 1000;
+}
+console.log(GenOTP())
+
+//code to genearate random number
+function random() {
+  return (Date.now() % 10)/10;
+}
+console.log(random());
