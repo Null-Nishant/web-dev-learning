90fc79db1aac0bd89a4ca0b6e05c9081f7c9481b
Nishant Kumar <nsingh9430@gmail.com>
2025-10-24 21:35:52 +0530

    Add files via upload

---
+++ js/day9.js
@@ -0,0 +1,18 @@
+//Document Object Model(DOM)
+// window Global Object
+// Selection of Element of HTML
+document.getElementById();
+
+document.getElementsByClassName();
+
+document.getElementsByTagName();
+
+document.querySelector(); // Give the first element. Argument is like CSS Selector
+
+document.querySelectorAll(); // Give the NodeList of elements. Argument is like CSS Selector
+
+// innerText  give the text only
+
+//innerHTML  give the everything inside the selected element
+
+// TextContent  give the all text inside the selected element even if it is hidden
